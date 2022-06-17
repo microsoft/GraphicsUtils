de08221e095810c32c746d48da51791cb6743f11
Yuxiao Guo <yuxgu@microsoft.com>
2022-06-17 18:28:39 +0800

    Remove short alias

--- __init__.py
+++ __init__.py
@@ -1,7 +1,2 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
-from . import string as g_str
-from . import stream_io as g_io
-from . import config as g_cfg
-from . import perf as g_perf
-from . import design as g_dsgn
